0e05056522fd2908459c863410d3b80432297231
git_LinexMugambi <linexmugambi254@gmail.com>
2023-01-24 14:00:17 +0300

    Fixed issues with checkout process

--- utils.js
+++ utils.js
@@ -2,3 +2,4 @@ Update utils.js with utility function 4
 Update utils.js with utility function 9
 Update utils.js with utility function 14
 Update utils.js with utility function 19
+Update utils.js with utility function 24
