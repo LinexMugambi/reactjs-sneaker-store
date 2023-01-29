43dde414aaacff5b46f5aea66b4793359501a6f2
git_LinexMugambi <linexmugambi254@gmail.com>
2023-01-29 14:00:17 +0300

    Fixed layout issues on product page

--- utils.js
+++ utils.js
@@ -3,3 +3,4 @@ Update utils.js with utility function 9
 Update utils.js with utility function 14
 Update utils.js with utility function 19
 Update utils.js with utility function 24
+Update utils.js with utility function 29
